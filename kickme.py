55280b926d8b2b3be9be35de0ca38106f0affd8c
kamikadze3113 <68239296+kamikadze3113@users.noreply.github.com>
2020-08-09 23:14:41 +0300

    Fix Tab #2

--- kickme.py
+++ kickme.py
@@ -19,7 +19,7 @@ class LeaveMod(loader.Module):
 			return
 		else:
 			GIF = ["https://i.gifer.com/1UEg.gif", "https://i.gifer.com/5Tp.gif", "https://i.gifer.com/Tia.gif", "https://i.gifer.com/7wD7.gif", "https://i.gifer.com/fya2.gif"]
-		    await message.client.send_file(message.to_id, random.choice(GIF))
-		    await message.edit(f"<b>{text}</b>")
+			await message.client.send_file(message.to_id, random.choice(GIF))
+			await message.edit(f"<b>{text}</b>")
 		await sleep(1)
 		await message.client(LeaveChannelRequest(message.chat_id))
